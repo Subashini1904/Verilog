e5d96b9fed921377bd9d3b85f10552f799e181b2
Subashini1904 <subharaju2004@gmail.com>
2025-12-10 20:30:05 +0530

    half_adder
    Implemented a half adder module with sum and carry outputs.

---
+++ DAY 3/Q5/halfadder.v
@@ -0,0 +1,16 @@
+module half_adder(
+    input  a,
+    input  b,
+    output sum,
+    output carry
+);
+    xor   (sum,   a, b);  
+    and   (carry, a, b);  
+endmodule
+
+
+//OUTPUT//
+Time=0 | a=0 b=0 | sum=0 carry=0
+Time=10 | a=0 b=1 | sum=1 carry=0
+Time=20 | a=1 b=0 | sum=1 carry=0
+Time=30 | a=1 b=1 | sum=0 carry=1
