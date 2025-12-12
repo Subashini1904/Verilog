74bb7e8a3edd3907d719d25d2cffa96c438f37ec
Subashini1904 <subharaju2004@gmail.com>
2025-12-12 12:13:17 +0530

    Add files via upload

---
+++ DAY 5/Q2/mux2to1_case_tb.v
@@ -0,0 +1,23 @@
+module mux2to1_case_tb;
+reg a;
+reg b;
+reg s;
+wire y;
+mux2to1_case dut (
+.a(a),
+.b(b),
+.s(s),
+.y(y)
+);
+initial begin
+    $dumpfile("mux2to1_case.vcd");
+    $dumpvars(0, mux2to1_case_tb);
+    $monitor("time=%0t | a=%b b=%b s=%b | y=%b", $time, a, b, s, y);
+    s = 0; a = 0; b = 0; #10;
+    s = 0; a = 0; b = 1; #10;
+    s = 1; a = 1; b = 0; #10;
+    s = 1; a = 1; b = 1; #10;
+    $finish;
+end
+endmodule
+
