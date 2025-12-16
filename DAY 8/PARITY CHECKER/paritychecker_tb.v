4166c1d64f9e0f31e9a0731dc92ee037967a4c69
Subashini1904 <subharaju2004@gmail.com>
2025-12-16 21:18:51 +0530

    paritychecker_tb

--- DAY 8/PARITY CHECKER/paritychecker_tb.v
+++ DAY 8/PARITY CHECKER/paritychecker_tb.v
@@ -1,18 +1,20 @@
 module parity_checker_tb;
 reg  [7:0] data;
-wire parity;
+reg parity;
+wire error;
 parity_checker dut (
 .data(data),
-.parity(parity)
+.parity(parity),
+.error(error)
 );
 initial begin
-    $monitor("time=%0t data=%b parity=%b", $time, data, parity);
-    data = 8'b00000000; #10; 
-    data = 8'b00000001; #10;
-    data = 8'b00000111; #10; 
-    data = 8'b11111111; #10; 
-    data = 8'b10101010; #10; 
+    $monitor("time=%0t data=%b parity=%b error=%b", $time, data, parity, error);
+    data = 8'b00000001; parity = 1'b1; #10;
+    data = 8'b00000111; parity = 1'b1; #10;
+    data = 8'b00000001; parity = 1'b0; #10;
+    data = 8'b00000111; parity = 1'b0; #10;
     $finish;
 end
 endmodule
+
 
