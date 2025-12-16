ad7dd084bd0d91ca1d5e7a8105b47578c2cb88af
Subashini1904 <subharaju2004@gmail.com>
2025-12-16 21:18:00 +0530

    paritychecker.v

--- DAY 8/PARITY CHECKER/paritychecker.v
+++ DAY 8/PARITY CHECKER/paritychecker.v
@@ -1,14 +1,14 @@
 module parity_checker (
 input  [7:0] data,
-output parity
+input        parity,
+output       error
 );
-assign parity = ^data;   
+assign error = ^{data, parity};
 endmodule
 
 
 //OUTPUT//
-time=0 data=00000000 parity=0
-time=10 data=00000001 parity=1
-time=20 data=00000111 parity=1
-time=30 data=11111111 parity=0
-time=40 data=10101010 parity=0
+time=0 data=00000001 parity=1 error=0
+time=10 data=00000111 parity=1 error=0
+time=20 data=00000001 parity=0 error=1
+time=30 data=00000111 parity=0 error=1
