dc0d416aec24755911b7b047af4c580fb803b4ff
Subashini1904 <subharaju2004@gmail.com>
2025-12-26 12:47:44 +0530

    Update up_down_counter.v

--- DAY_14/UP DOWN COUNTER/up_down_counter.v
+++ DAY_14/UP DOWN COUNTER/up_down_counter.v
@@ -14,3 +14,16 @@ always @(posedge clk) begin
 end
 endmodule
 
+//OUTPUT//
+Time=0 up_down=1 count=xxxx
+Time=5 up_down=1 count=0000
+Time=15 up_down=1 count=0001
+Time=25 up_down=1 count=0010
+Time=35 up_down=1 count=0011
+Time=45 up_down=1 count=0100
+Time=55 up_down=1 count=0101
+Time=60 up_down=0 count=0101
+Time=65 up_down=0 count=0100
+Time=75 up_down=0 count=0011
+Time=85 up_down=0 count=0010
+Time=95 up_down=0 count=0001
